3a7766dcfd182f38fce9b4f9f204e399cb51ff1f
FlorianDHaene <dhaene_florian@hotmail.com>
2018-12-04 18:31:19 +0100

    Florian - Beheren

--- app/src/main/java/be/thomasmore/funetics/BeherenActivity.java
+++ app/src/main/java/be/thomasmore/funetics/BeherenActivity.java
@@ -4,15 +4,106 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class BeherenActivity extends AppCompatActivity {
+
+    private DatabaseHelper db;
+    private Groep selectedGroep = null;
+    private Kind selectedKind = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beheren);
+
+        db = new DatabaseHelper(this);
+
+        leesGroepen();
+        leesKinderenWhereGroep(0);
     }
 
+    private void leesGroepen(){
+        final List<Groep> groepen = db.getGroepen();
+
+        ArrayAdapter<Groep> adapter =
+                new ArrayAdapter<Groep>(this,
+                        android.R.layout.simple_list_item_1, groepen);
+
+        final ListView listViewGroepen =
+                (ListView) findViewById(R.id.listViewGroepen);
+        listViewGroepen.setAdapter(adapter);
+
+        listViewGroepen.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parentView,
+                                            View childView, int position, long id) {
+                        selectedGroep = groepen.get(position);
+                        int selectedGroepId = (int) selectedGroep.getId();
+                        leesKinderenWhereGroep(selectedGroepId);
+                    }
+                });
+
+//        listViewGroepen.setOnItemClickListener(){
+//            @Override
+//            public void onItemClick(AdapterView<?> parentView, View childView, int position, long id) {
+//                int selectedGroep = (int) groepen.get(position).getId();
+//                leesKinderenWhereGroep(selectedGroep);
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parentView) {
+//                // your code here
+//            }
+//        });
+
+//        ArrayAdapter<Groep> aa = new ArrayAdapter(this,android.R.layout.simple_list_item_1, groepen);
+//        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+//        listViewGroepen.setAdapter(aa);
+    }
+
+    private void leesKinderenWhereGroep(int groepId){
+
+        List<Kind> kinderenLijst;
+
+        if(groepId == 0){
+            kinderenLijst = db.getKinderen();
+
+        }else{
+            kinderenLijst = db.getKinderenWhereGroepId(groepId);
+        }
+
+        final List<Kind> kinderen = kinderenLijst;
+
+        final ListView listViewKinderen = (ListView) findViewById(R.id.listViewKinderen);
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_list_item_1, kinderen);
+        listViewKinderen.setAdapter(aa);
+
+        listViewKinderen.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parentView,
+                                            View childView, int position, long id) {
+                        selectedKind = kinderen.get(position);
+                        long selectedKindId = (long) selectedKind.getId();
+                        leesKindDetail(selectedKindId);
+                    }
+                });
+
+    }
+
+    private void leesKindDetail(long kindId){
+
+        final Kind kind = db.getKindById(kindId);
+    }
 }
--- app/src/main/java/be/thomasmore/funetics/MainActivity.java
+++ app/src/main/java/be/thomasmore/funetics/MainActivity.java
@@ -91,36 +91,9 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(aa);
     }
 
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-
-//    public void goDetail_onClick(View v) {
-////        Bundle bundle = new Bundle();
-////        bundle.putString("naam", "Jeff Hermans");
-////        bundle.putLong("groep", 1);
-//
-//        Intent intent = new Intent(this, DetailActivity.class);
-////        intent.putExtras(bundle);
-//
-//        startActivity(intent);
-//
-////        startActivityForResult(intent, 1);  // 1 is requestCode
-//    }
-
     public void goBeheren_onClick(View v) {
         Intent intent = new Intent(this, BeherenActivity.class);
-
         startActivity(intent);
-
-//        startActivityForResult(intent, 1);  // 1 is requestCode
     }
 
     public void goDetail_onClick(View v) {
