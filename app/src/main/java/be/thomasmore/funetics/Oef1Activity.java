4d422f62b3a57ec6622f0948cd7aeb65b88643c9
FlorianDHaene <dhaene_florian@hotmail.com>
2018-12-17 13:20:45 +0100

    oef 1

--- app/src/main/java/be/thomasmore/funetics/Oef1Activity.java
+++ app/src/main/java/be/thomasmore/funetics/Oef1Activity.java
@@ -2,16 +2,12 @@ package be.thomasmore.funetics;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class Oef1Activity extends AppCompatActivity implements MediaPlayer.OnCompletionListener{
 
     private MediaPlayer audioPlayer = null;
-    int[] tracks = new int[4];
+    int[] tracks = new int[3];
     int currentTrack = 0;
 
     @Override
@@ -25,9 +21,9 @@ public class Oef1Activity extends AppCompatActivity implements MediaPlayer.OnCom
 
     public void setAudioPlayer() {
         tracks[0] = R.raw.oef1_wat_zie_je_hier;
-        tracks[1] = R.raw.duikbril;                         // Oefenwoord
+        tracks[1] = R.raw.duikbril;                     // Oefenwoord
         tracks[2] = R.raw.oef1_weet_je_wat_dat_is;
-        tracks[3] = R.raw.duikbril_explain;                 // Uitleg
+        tracks[3] = R.raw.duikbril_def;                 // Uitleg
         audioPlayer = MediaPlayer.create(getApplicationContext(), tracks[currentTrack]);
         audioPlayer.setOnCompletionListener(this);
     }
