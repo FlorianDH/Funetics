9c78bd90946d7dcbf6c3e32f107a0d500d797a05
Stef Goor <r0663065@student.thomasmore.be>
2019-01-03 17:18:11 +0100

    Stef - Oefening 6.2

--- app/src/main/java/be/thomasmore/funetics/Oef6_2Activity.java
+++ app/src/main/java/be/thomasmore/funetics/Oef6_2Activity.java
@@ -4,6 +4,7 @@ import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,18 +16,19 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class Oef6_2Activity extends AppCompatActivity implements MediaPlayer.OnCompletionListener {
+    private MediaPlayer zoemPlayer = null;
     private MediaPlayer audioPlayer = null;
     private int[] tracks = new int[3];
+    private int[] tracks2 = new int[1];
     private int currentTrack = 0;
-
-    public int score = 0;
-    public int aantalPogingen = 0;
+    private int zoemDuur;
 
     private DatabaseHelper db;
     private Doelwoord huidigDoelwoord;
     private Kind huidigKind;
 
     private boolean isPlaying = false; //false by default
+    private boolean isPlaying2 = false; //false by default
 
     private ObjectAnimator bijAnimation = new ObjectAnimator();
 
@@ -45,6 +47,8 @@ public class Oef6_2Activity extends AppCompatActivity implements MediaPlayer.OnC
         huidigKind = db.getKindById(kindId);
 
         setAudioPlayer();
+        setZoemPlayer();
+        zoemDuur = zoemPlayer.getDuration();
         playAudioPlayer();
 
         TextView textNaamKind = (TextView) findViewById(R.id.textViewNaam);
@@ -60,7 +64,21 @@ public class Oef6_2Activity extends AppCompatActivity implements MediaPlayer.OnC
         soundFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                bijAnimation.start();
                 playButton_onClick(view);
+            }
+        });
+
+        FloatingActionButton nextFab = (FloatingActionButton) findViewById(R.id.nextFAB);
+        nextFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Terug naar oefening activity
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("score", String.valueOf(1));
+                returnIntent.putExtra("aantalPogingen", String.valueOf(1));
+                setResult(Activity.RESULT_OK,returnIntent);
+                finish();
             }
         });
 
@@ -69,20 +87,28 @@ public class Oef6_2Activity extends AppCompatActivity implements MediaPlayer.OnC
         vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                float afstand = (float) layout.getMeasuredWidth();
+                float afstand = (float) layout.getMeasuredWidth() - 60;
 
                 ImageView afbeeldingBij = (ImageView) findViewById(R.id.imageViewBij);
                 bijAnimation = ObjectAnimator.ofFloat(afbeeldingBij, "translationX", afstand);
-                bijAnimation.setDuration(3000);
+                bijAnimation.setDuration(zoemDuur);
                 bijAnimation.start();
             }
         });
     }
 
     public void setAudioPlayer() {
-        tracks[0] = getResources().getIdentifier(huidigDoelwoord.getNaam().toLowerCase() + "_6_2", "raw", getPackageName()); //oefenwoord_6_2
+        tracks[0] = getResources().getIdentifier(huidigDoelwoord.getNaam().toLowerCase(), "raw", getPackageName());
+        //tracks[0] = getResources().getIdentifier(huidigDoelwoord.getNaam().toLowerCase() + "_6_2", "raw", getPackageName()); //oefenwoord_6_2
         tracks[1] = R.raw.oef6_2;
-        tracks[2] = getResources().getIdentifier(huidigDoelwoord.getNaam().toLowerCase() + "_6_2", "raw", getPackageName()); //oefenwoord_6_2
+        tracks[2] = getResources().getIdentifier(huidigDoelwoord.getNaam().toLowerCase(), "raw", getPackageName());
+        //tracks[2] = getResources().getIdentifier(huidigDoelwoord.getNaam().toLowerCase() + "_6_2", "raw", getPackageName()); //oefenwoord_6_2
+    }
+
+    public void setZoemPlayer() {
+        zoemPlayer = MediaPlayer.create(this, getResources().getIdentifier(huidigDoelwoord.getNaam().toLowerCase(), "raw", getPackageName()));
+        //tracks2[0] = getResources().getIdentifier(huidigDoelwoord.getNaam().toLowerCase(), "raw", getPackageName());
+        //tracks[0] = getResources().getIdentifier(huidigDoelwoord.getNaam().toLowerCase() + "_6_2", "raw", getPackageName()); //oefenwoord_6_2
     }
 
     public void playAudioPlayer(){
@@ -92,8 +118,17 @@ public class Oef6_2Activity extends AppCompatActivity implements MediaPlayer.OnC
         audioPlayer.start();
     }
 
+    public void playZoemPlayer(){
+        isPlaying2 = true;
+        zoemPlayer.start();
+    }
+
     public void onCompletion(MediaPlayer audioPlayer2) {
         audioPlayer2.release();
+        if (currentTrack == 1){
+            bijAnimation.start();
+        }
+
         if (currentTrack < tracks.length-1) {
             isPlaying = true;
             currentTrack++;
@@ -110,9 +145,8 @@ public class Oef6_2Activity extends AppCompatActivity implements MediaPlayer.OnC
             audioPlayer.stop();
         }
 
-        isPlaying = true;
-        currentTrack = 0;
-        playAudioPlayer();
+        isPlaying2 = true;
+        playZoemPlayer();
     }
 
 }
