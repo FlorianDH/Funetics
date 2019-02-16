6c4207fb60f640573d482289f45dc6edabf02778
Stef Goor <r0663065@student.thomasmore.be>
2019-02-16 17:16:30 +0100

    Stef - Scores nameting tonen af

--- app/src/main/java/be/thomasmore/funetics/WoordAdapter.java
+++ app/src/main/java/be/thomasmore/funetics/WoordAdapter.java
@@ -1,5 +1,6 @@
 package be.thomasmore.funetics;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
@@ -32,6 +33,7 @@ public class WoordAdapter extends ArrayAdapter<GetestWoord> {
 
         final TextView textViewWoord = (TextView) rowView.findViewById(R.id.textViewWoord);
 
+        //Textviews van de resultaten definieren
         final TextView textViewVoormeting = (TextView) rowView.findViewById(R.id.textViewVoormetingResultaat);
         final TextView textViewOef1 = (TextView) rowView.findViewById(R.id.textViewOef1Resultaat);
         final TextView textViewOef2 = (TextView) rowView.findViewById(R.id.textViewOef2Resultaat);
@@ -42,12 +44,27 @@ public class WoordAdapter extends ArrayAdapter<GetestWoord> {
         final TextView textViewOef6Naam = (TextView) rowView.findViewById(R.id.textViewOef6);
         final TextView textViewOefNameting = (TextView) rowView.findViewById(R.id.textViewNametingResultaat);
 
+        //Textviews van de resultaten default op niet voltooid zetten
+        textViewVoormeting.setText("Niet voltooid");
+        textViewVoormeting.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
+        textViewOef1.setText("Niet voltooid");
+        textViewOef1.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
+        textViewOef2.setText("Niet voltooid");
+        textViewOef2.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
+        textViewOef3.setText("Niet voltooid");
+        textViewOef3.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
+        textViewOef4.setText("Niet voltooid");
+        textViewOef4.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
+        textViewOef5.setText("Niet voltooid");
+        textViewOef5.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
+        textViewOef6.setText("Niet voltooid");
+        textViewOef6.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
+        textViewOefNameting.setText("Niet voltooid");
+        textViewOefNameting.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
+
         db = new DatabaseHelper(context);
         GetestWoord getestWoord = values.get(position);
         Doelwoord doelwoord = db.getDoelwoordById(getestWoord.getDoelwoordId());
-
-//        Test test = db.getTestById(getestWoord.getTestId());
-//        int conditieId = test.getConditieId();
 
         //Oefenwoord zetten achter duibril
         if (doelwoord.getId() == 0){
@@ -61,207 +78,106 @@ public class WoordAdapter extends ArrayAdapter<GetestWoord> {
         List<GetesteOefening> oefeningen = new ArrayList<GetesteOefening>();
         oefeningen = db.getGetesteOefeningenWhereGetestWoordId((int) getestWoord.getId());
 
+        //Alle resultaten bij de juiste oefening zetten
         for (GetesteOefening getesteOefening: oefeningen){
             switch (getesteOefening.getOefeningId()){
                 case 0:
-
-            }
-        }
-
-        //Alle resultaten bij de juiste oefening zetten
-        //Voormeting
-        GetesteOefening oefening0 = null;
-        try {
-            oefening0 = oefeningen.get(0);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (oefening0 == null){
-            textViewVoormeting.setText("Niet voltooid");
-            textViewVoormeting.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
-        }
-        else {
-            if (oefening0.getScore() == 1){
-                textViewVoormeting.setText("Juist");
-                textViewVoormeting.setTextColor(ContextCompat.getColor(context, R.color.color_green));
-            }
-            else {
-                textViewVoormeting.setText("Fout");
-                textViewVoormeting.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
-            }
-        }
-
-        //Oefening 1
-        GetesteOefening oefening1 = null;
-        try {
-            oefening1 = oefeningen.get(1);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (oefening1 == null){
-            textViewOef1.setText("Niet voltooid");
-            textViewOef1.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
-        }
-        else {
-            if (oefening1.getScore() == 1){
-                textViewOef1.setText("Voltooid");
-                textViewOef1.setTextColor(ContextCompat.getColor(context, R.color.color_green));
-            }
-        }
-
-        //Oefening 2
-        GetesteOefening oefening2 = null;
-        try {
-            oefening2 = oefeningen.get(2);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (oefening2 == null){
-            textViewOef2.setText("Niet voltooid");
-            textViewOef2.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
-        }
-        else {
-            if (oefening2.getScore() == 1){
-                textViewOef2.setText("Juiste uitspraak");
-                textViewOef2.setTextColor(ContextCompat.getColor(context, R.color.color_green));
-            }
-            else if (oefening2.getScore() == 0 && oefening2.getAantalPogingen() == 1){
-                textViewOef2.setText("Foute uitspraak");
-                textViewOef2.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
-            }
-        }
-
-        //Oefening 3
-        GetesteOefening oefening3 = null;
-        try {
-            oefening3 = oefeningen.get(3);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (oefening3 == null){
-            textViewOef3.setText("Niet voltooid");
-            textViewOef3.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
-        }
-        else {
-            textViewOef3.setText(oefening3.getScore() + " / " + oefening3.getAantalPogingen());
-            if (oefening3.getScore() == 2){
-                textViewOef3.setTextColor(ContextCompat.getColor(context, R.color.color_green));
-            }
-            else {
-                textViewOef3.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
-            }
-        }
-
-        //Oefening 4
-        GetesteOefening oefening4 = null;
-        try {
-            oefening4 = oefeningen.get(4);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (oefening4 == null){
-            textViewOef4.setText("Niet voltooid");
-            textViewOef4.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
-        }
-        else {
-            textViewOef4.setText(oefening4.getScore() + " / " + oefening4.getAantalPogingen());
-            if (oefening4.getAantalPogingen() > 1){
-                textViewOef4.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
-            }
-            else {
-                textViewOef4.setTextColor(ContextCompat.getColor(context, R.color.color_green));
-            }
-        }
-
-        //Oefening 5
-        GetesteOefening oefening5 = null;
-        try {
-            oefening5 = oefeningen.get(5);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (oefening5 == null){
-            textViewOef5.setText("Niet voltooid");
-            textViewOef5.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
-        }
-        else {
-            textViewOef5.setText(oefening5.getScore() + " / " + oefening5.getAantalPogingen());
-            if (oefening5.getAantalPogingen() > 1){
-                textViewOef5.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
-            }
-            else {
-                textViewOef5.setTextColor(ContextCompat.getColor(context, R.color.color_green));
-            }
-        }
-
-        //Oefening 6
-        GetesteOefening oefening6 = null;
-        try {
-            oefening6 = oefeningen.get(6);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (oefening6 == null){
-            textViewOef6.setText("Niet voltooid");
-            textViewOef6.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
-        }
-        else {
-            //Oefening 6.1
-            if (oefening6.getOefeningId() == 6){
-                textViewOef6Naam.setText("Oefening 6.1: ");
-                if (oefening6.getScore() == 1){
-                    textViewOef6.setText("Goed gevoel");
+                    //Voormeting
+                        if (getesteOefening.getScore() == 1){
+                            textViewVoormeting.setText("Juist");
+                            textViewVoormeting.setTextColor(ContextCompat.getColor(context, R.color.color_green));
+                        }
+                        else {
+                            textViewVoormeting.setText("Fout");
+                            textViewVoormeting.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
+                        }
+                        break;
+                case 1:
+                    //Oefening 1
+                        if (getesteOefening.getScore() == 1){
+                            textViewOef1.setText("Voltooid");
+                            textViewOef1.setTextColor(ContextCompat.getColor(context, R.color.color_green));
+                        }
+                        break;
+                case 2:
+                    //Oefening 2
+                    if (getesteOefening.getScore() == 1){
+                        textViewOef2.setText("Juiste uitspraak");
+                        textViewOef2.setTextColor(ContextCompat.getColor(context, R.color.color_green));
+                    }
+                    else if (getesteOefening.getScore() == 0 && getesteOefening.getAantalPogingen() == 1){
+                        textViewOef2.setText("Foute uitspraak");
+                        textViewOef2.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
+                    }
+                    break;
+                case 3:
+                    //Oefening 3
+                    textViewOef3.setText(getesteOefening.getScore() + " / " + getesteOefening.getAantalPogingen());
+                    if (getesteOefening.getScore() == 2){
+                        textViewOef3.setTextColor(ContextCompat.getColor(context, R.color.color_green));
+                    }
+                    else {
+                        textViewOef3.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
+                    }
+                    break;
+                case 4:
+                    //Oefening 4
+                    textViewOef4.setText(getesteOefening.getScore() + " / " + getesteOefening.getAantalPogingen());
+                    if (getesteOefening.getAantalPogingen() > 1){
+                        textViewOef4.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
+                    }
+                    else {
+                        textViewOef4.setTextColor(ContextCompat.getColor(context, R.color.color_green));
+                    }
+                    break;
+                case 5:
+                    //Oefening 5
+                    textViewOef5.setText(getesteOefening.getScore() + " / " + getesteOefening.getAantalPogingen());
+                    if (getesteOefening.getAantalPogingen() > 1){
+                        textViewOef5.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
+                    }
+                    else {
+                        textViewOef5.setTextColor(ContextCompat.getColor(context, R.color.color_green));
+                    }
+                    break;
+                case 6:
+                    //Oefening 6.1
+                        textViewOef6Naam.setText("Oefening 6.1: ");
+                        if (getesteOefening.getScore() == 1){
+                            textViewOef6.setText("Goed gevoel");
+                            textViewOef6.setTextColor(ContextCompat.getColor(context, R.color.color_green));
+                        }
+                        else {
+                            textViewOef6.setText("Slecht gevoel");
+                            textViewOef6.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
+                        }
+                    break;
+                case 7:
+                    //Oefening 6.2
+                        textViewOef6Naam.setText("Oefening 6.2: ");
+                        textViewOef6.setText("Voltooid");
+                        textViewOef6.setTextColor(ContextCompat.getColor(context, R.color.color_green));
+                        break;
+                case 8:
+                    //Oefening 6.3
+                    textViewOef6Naam.setText("Oefening 6.3: ");
+                    textViewOef6.setText("Voltooid");
                     textViewOef6.setTextColor(ContextCompat.getColor(context, R.color.color_green));
-                }
-                else {
-                    textViewOef6.setText("Slecht gevoel");
-                    textViewOef6.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
-                }
-            }
-            //Oefening 6.2
-            else if (oefening6.getOefeningId() == 7){
-                textViewOef6Naam.setText("Oefening 6.2: ");
-                textViewOef6.setText("Voltooid");
-                textViewOef6.setTextColor(ContextCompat.getColor(context, R.color.color_green));
-            }
-            //Oefening 6.3
-            else if (oefening6.getOefeningId() == 8){
-                textViewOef6Naam.setText("Oefening 6.3: ");
-                textViewOef6.setText("Voltooid");
-                textViewOef6.setTextColor(ContextCompat.getColor(context, R.color.color_green));
+                    break;
+                case 9:
+                    //Nameting
+                    if (getesteOefening.getScore() == 1){
+                        textViewOefNameting.setText("Juist");
+                        textViewOefNameting.setTextColor(ContextCompat.getColor(context, R.color.color_green));
+                    }
+                    else {
+                        textViewOefNameting.setText("Fout");
+                        textViewOefNameting.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
+                    }
+                default:
+                    break;
             }
         }
-
-        //Nameting
-        GetesteOefening oefening7 = null;
-        try {
-            oefening7 = oefeningen.get(7);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (oefening7 == null){
-            textViewOefNameting.setText("Niet voltooid");
-            textViewOefNameting.setTextColor(ContextCompat.getColor(context, R.color.color_red_dark));
-        }
-        else {
-            if (oefening7.getScore() == 1){
-                textViewOefNameting.setText("Juist");
-                textViewOefNameting.setTextColor(ContextCompat.getColor(context, R.color.color_green));
-            }
-            else {
-                textViewOefNameting.setText("Fout");
-                textViewOefNameting.setTextColor(ContextCompat.getColor(context, R.color.color_orange_dark));
-            }
-        }
-
         return rowView;
     }
 }
